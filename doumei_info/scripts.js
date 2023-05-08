a63266db2aa99a4723800dc7ce5dc4a88812f6dc
text89 <gotz.game.records@gmail.com>
2023-05-08 16:02:51 +0900

    add date description

--- doumei_info/scripts.js
+++ doumei_info/scripts.js
@@ -291,6 +291,8 @@ function displayTopPowers(data) {
         data.push(...csvData);
     }
     if (! data.length == 0){
+        var unionMemberPower = document.getElementById("union-member-power");
+        unionMemberPower.textContent = "総合力 (" + data[0]["date"] + '時点)';
         const powerData = document.getElementById("power-histogram");
         powerData.textContent = "";
         // createHistogram(data, d => +d.level, 1, "#level-histogram", levelMargin);
